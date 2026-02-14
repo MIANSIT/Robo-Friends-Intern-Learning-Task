2637ecaef39838b044fb6d5a2fc51f931a4f3be0
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-14 15:50:31 +0600

    :sparkles: Feat: Added favorite function to select card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import {FaUser,FaRobot,FaIdBadge} from "react-icons/fa";
 import {MdEmail,MdCake} from "react-icons/md";
 import { IconContext } from "react-icons/lib";
 import PropTypes from "prop-types";
 import 'tachyons';
 import '../App.css';
+import classNames from "classnames";
 
 function  Card({id,name,username,email}){
+    const [isfav,setFav]=useState(false);
+    const cardClass=classNames('Cardrobotcard',{'fav':isfav});
 
     return(
     
         <IconContext.Provider value={{color:"white", className:"global-class-name" ,size:"22px"}}>
-            <div className="Cardrobotcard">
+            <div className={cardClass}onClick={()=>
+                    setFav(!isfav)}>  
                 <div className="Cardrobotimgcontainer">
                     <img className="Cradrobotimg" src={`https://robohash.org/${id}?size=200x200`} alt="robot"/>
                 </div>
@@ -22,6 +26,7 @@ function  Card({id,name,username,email}){
             </div>
           
             </div>
+        
         </IconContext.Provider>
         
 
